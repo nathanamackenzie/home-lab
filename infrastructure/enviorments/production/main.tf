c938eed5d1b3d025f527b14d493a6f44f13ee048
nathanamackenzie <nathanamackenzie@gmail.com>
2024-02-29 22:22:53 -0500

    S3 static website added to production

--- infrastructure/enviorments/production/main.tf
+++ infrastructure/enviorments/production/main.tf
@@ -58,3 +58,47 @@ module "prod-use2-vpc1" {
     ["192.168.14.0/24", "us-east-2b", "pvt-db-2b"],
     ["192.168.15.0/24", "us-east-2b", "pvt-res-2b"]]
 }
+
+
+// Static website
+resource "aws_s3_bucket" "personal-website" {
+  bucket = "personal-website-6378"
+}
+
+resource "aws_s3_bucket_policy" "website-bucket-policy" {
+  bucket = aws_s3_bucket.personal-website.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.personal-website.id}/*"
+            ]
+        }
+    ]
+})
+}
+
+resource "aws_s3_bucket_public_access_block" "allow-public" {
+  bucket = aws_s3_bucket.personal-website.id
+  block_public_acls = false
+  block_public_policy = false
+}
+
+resource "aws_s3_bucket_website_configuration" "hosting" {
+  bucket = aws_s3_bucket.personal-website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
